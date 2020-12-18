606a47f45cf50caea80247575ac5a73ce98dc0cc
joelle.helgert <joelle.helgert@gmail.com>
2020-12-18 14:54:43 +0100

    Implement Basic Step Visualizatio

--- StepByStepInstructions/Assets/Scripts/RecipeManager.cs
+++ StepByStepInstructions/Assets/Scripts/RecipeManager.cs
@@ -6,6 +6,8 @@ using System;
 public class RecipeManager : MonoBehaviour
 {
     Recipe recipe;
+    [SerializeField]
+    public UIInteractables UI;
 
     void Start()
     {
@@ -28,21 +30,21 @@ public class RecipeManager : MonoBehaviour
 
     private void LoadIngredients(string path)
     {
-        Debug.LogWarning("path: " + path);
         TextAsset data = Resources.Load<TextAsset>(path);
         string[] dataRows = data.text.Split(new char[] { '\n' });
 
         for (int i = 1; i < dataRows.Length - 1; i++)
         {
             string[] row = dataRows[i].Split(new char[] { ';' });
-            Ingredient ingredient = new Ingredient(float.Parse(row[0]), row[1], row[2], row[3]);
+            Ingredient ingredient = new Ingredient(float.Parse(row[0]), row[1], row[2], row[3].Trim());
             recipe.Ingredients.Add(ingredient);
         }
+
+        UI.PaintIngredients(recipe.Ingredients);
     }
 
     private void LoadInstructions(string path)
     {
-        Debug.Log("--------------- load instructions");
         TextAsset data = Resources.Load<TextAsset>(path);
         string[] dataRows = data.text.Split(new char[] { '\n' });
 
@@ -50,11 +52,13 @@ public class RecipeManager : MonoBehaviour
         {
             string[] row = dataRows[i].Split(new char[] { ';' });
             StepType type = (StepType)Enum.Parse(typeof(StepType), row[2]);
-            Debug.LogWarning("row 4 " + row[4] + " length: " + row[4].Length);
-            float f;
-            int? seconds = float.TryParse(row[4], out f) ? (int?)f : null; 
+            int? seconds = float.TryParse(row[4], out float f) ? (int?)f : null;
             Step step = new Step(float.Parse(row[0]), row[1], type, row[3], seconds);
+            recipe.Instructions.Add(step);
 
         }
+
+        Debug.Log("START");
+        UI.EnableStart(recipe.Instructions);
     }
 }
--- StepByStepInstructions/Assets/Scripts/UIInteractables.cs
+++ StepByStepInstructions/Assets/Scripts/UIInteractables.cs
@@ -1,29 +1,111 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIInteractables : MonoBehaviour
 {
     public GameObject Ingredients;
-    public GameObject Data; // probs not GameObject
+    public TextMeshProUGUI Dough;
+    public TextMeshProUGUI Filling;
+    public TextMeshProUGUI Sauce;
+    public Button StartButton;
+    public GameObject Step;
+    public TextMeshProUGUI StepField;
+    public Button NextStepButton;
 
     public GameObject HeadlinePrefab;
-    public GameObject IngredientPrefab;
+
+    private List<Step> Instructions;
+    private IEnumerator<Step> StepEnumerator;
+    private bool UpdateUI;
 
     private void Start()
     {
         Ingredients.SetActive(false);
+        UpdateUI = false;
+        // StartButton.interactable = false;
         // fill ingredients in textfield
-        PaintIngredients();
         // hide textfield & close
+    }
+
+    private void Update()
+    {
+        if(UpdateUI && StepEnumerator.Current != null)
+        {
+            // check type of instruction
+            // paint
+            switch(StepEnumerator.Current.Type)
+            {
+                case StepType.Onions:
+                    break;
+                case StepType.Heading:
+                    StepField.text = "HEADING";
+                    break;
+                case StepType.Timer:
+                    break;
+                case StepType.Circles:
+                    break;
+                case StepType.BasicStep:
+                default:
+                    StepField.text = StepEnumerator.Current.Number + ". " +StepEnumerator.Current.Description;
+                    break;
+            }
+
+            UpdateUI= false;
+        } else if (UpdateUI && StepEnumerator.Current == null)
+        {
+            StepField.text = "DONE";
+            UpdateUI = false;
+            NextStepButton.interactable = false;
+        }
     }
 
     public void OpenIngredients() => Ingredients.SetActive(true);
     public void CloseIngredients() => Ingredients.SetActive(false);
 
-    public void PaintIngredients()
+    public void PaintIngredients(List<Ingredient> ingredients)
     {
-        // TODO
+        /*
+        Debug.Log(GameObject.Find("Dough"));
+        GameObject.Find("Dough").TryGetComponent<TextMeshPro>(out TextMeshPro Dough);
+        Ingredients.GetComponent("Filling").TryGetComponent<TextMeshPro>(out TextMeshPro Filling);
+        Ingredients.GetComponent("Sauce").TryGetComponent<TextMeshPro>(out TextMeshPro Sauce);
+        */
+        
+        foreach (Ingredient ingredient in ingredients) {
+            switch (ingredient.Part)
+            {
+                case "Dough":
+                    Dough.text = Dough.text + ingredient.Name + "\n";
+                    break;
+                case "Filling":
+                    Filling.text = Filling.text + ingredient.Name + "\n";
+                    break;
+                case "Sauce":
+                    Sauce.text = Sauce.text + ingredient.Name + "\n";
+                    break;
+            }
+        }
+    }
+
+    public void EnableStart(List<Step> instructions) {
+        StartButton.interactable = true;
+        Instructions = instructions;
+    }
+
+    public void StartCooking() 
+    {
+        StartButton.gameObject.SetActive(false);
+        StepEnumerator = Instructions.GetEnumerator();
+        StepEnumerator.MoveNext();
+        UpdateUI = true;
+    }
+
+    public void NextStep()
+    {
+        UpdateUI = true;
+        StepEnumerator.MoveNext();
     }
 }
